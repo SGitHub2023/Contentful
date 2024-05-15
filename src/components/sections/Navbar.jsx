97cd24d46bbf4a27b71f7ac49c933e100525862f
tdo leipzig <tdo.leipzig@gmail.com>
2024-05-15 17:54:29 +0200

    navbar changes

--- src/components/sections/Navbar.jsx
+++ src/components/sections/Navbar.jsx
@@ -5,37 +5,31 @@ import useIsMobile from "../../utilities/mobileCheck.js";
 
 function Navbar() {
 
-	const isMobile = useIsMobile(576);
+	const isMobile = useIsMobile(640);
+	const smoothScroll = (el) => el.scrollIntoView({ behavior: 'smooth'});
 
-	const smoothScroll = (el) => {
-		el.scrollIntoView({ behavior: 'smooth'});
-	}
+	const nav = (
+		<ul>
+			<li><Link to='/'>Home</Link></li>
+			<li><HashLink to='/#projects' scroll={smoothScroll}>Projects</HashLink></li>
+			<li><HashLink to='/#about-me' scroll={smoothScroll}>About me</HashLink></li>
+			<li><Link to='/contact'>Contact</Link></li>
+		</ul>
+	);
 
   return (
-    <div className="container mx-auto max-w-[1200px]">
-
-			<div className="flex items-center p-4">
-
+    <div className="container mx-auto max-w-[1200px] relative">
+			<div className="flex items-center p-4 bg-teal-900/10">
 				<button className="flex-1 sm:hidden">
 					<Icon icon="ion:menu-outline" className="text-3xl"/>
 					<span className="sr-only">Toggle Menu</span>
 				</button>
-
-				<nav className={isMobile ? 'main-nav mobile-nav' : 'main-nav desktop-nav'}>
-					<ul className="flex gap-6">
-						<li><Link to='/'>Home</Link></li>
-						<li><HashLink to='/#projects' scroll={smoothScroll}>Projects</HashLink></li>
-						<li><HashLink to='/#about-me' scroll={smoothScroll}>About me</HashLink></li>
-						<li><Link to='/contact'>Contact</Link></li>
-					</ul>
-				</nav>
-
+				{!isMobile && <nav className="desktop-nav">{nav}</nav>}
 				<button className="flex gap-3 px-4 py-[0.3rem] items-center rounded-full border-white border-2 text-xs uppercase">
 					<Icon icon="mdi:tray-download" className="text-lg"/><span>Download CV</span>
 				</button>
-
 			</div>
-
+			{isMobile && <nav className="mobile-nav">{nav}</nav>}
     </div>
   );
 }
