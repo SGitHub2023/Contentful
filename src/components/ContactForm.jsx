d73e2c922bda7972ad24fa2e5dc74ddb57c31b21
SGitHub2023 <sabrina.i.gerhardt@gmail.com>
2024-05-16 14:32:23 +0200

    Test4

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,26 @@
-function ContactForm() {
-  return (
-		<div className="bg-black/10">ContactForm</div>
+// function ContactForm() {
+//   return (
+// 		<div className="bg-black/10">ContactForm</div>
+// 	);
+// }
+
+// export default ContactForm;
+
+function ContactForm({ jotFormUrl }) {
+	return (
+		<div className="grid grid-cols-2 gap-4">
+			<div className="flex items-center justify-center p-4">
+				<p>Text</p>
+			</div>
+			<div className="p-4">
+				<iframe
+					src={jotFormUrl}
+					width="100%"
+					height="1500"
+					style={{ border: "none" }}
+				></iframe>
+			</div>
+		</div>
 	);
 }
 
