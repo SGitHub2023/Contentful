9acb11847108dd2f90c23c4e363a3c335b0b24e7
Hanno Bielz <bielzhanno@gmail.com>
2024-05-16 14:59:34 +0200

    added new styling to ProjectPage

--- src/components/routes/Project.jsx
+++ src/components/routes/Project.jsx
@@ -43,27 +43,28 @@ function Project() {
   const loadingTemplate = <p>Loading...</p>;
 
   const displayTemplate = (
-    <div className="Project flex flex-col lg:flex-row">
-      <div className="Project lg:w-1/3 bg-teal-950 pr-6 sm:text-center">
-        <div className="Project p-6 text-white">
-          <h2 className="text-lg font-bold mb-4">{project.title}</h2>
-          <div className="separator"></div>
+    <div className="project-overview flex flex-col md:flex-row">
+      <div className="md:w-1/3 bg-teal-950 pr-6 sm:text-center">
+        <div className="left-content py-7 text-white">
+          <h2 className="text-xl font-bold mb-4">{project.title}</h2>
           {rtCnt2 && <RichtextContent content={rtCnt2} />}
         </div>
       </div>
-      <div className="Project lg:w-2/3 bg-white p-6 flex justify-center items-center">
+      <div className="md:w-2/3 bg-white p-6 flex justify-center items-center">
         <div>
           <a
             href={project.projectLink}
             target="_blank"
             rel="noopener noreferrer"
+            className="project-link"
           >
             <img
               src={featuredImageUrl}
               alt={project.title}
-              className="mb-4 mx-auto cursor-pointer"
+              className="project-img mb-4 mx-auto cursor-pointer"
             />
           </a>
+
           {rtCnt1 && <RichtextContent content={rtCnt1} />}
 
           {rtCnt3 && <RichtextContent content={rtCnt3} />}
