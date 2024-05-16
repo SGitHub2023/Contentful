5b410f2a1b5e84aa7d06cd03997dedb7983b45af
AlexHess <alexhess104@gmail.com>
2024-05-16 10:52:49 +0200

    erste stylings für die ProjectSection, Projectcard und Experience auch angepasst

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,17 +1,21 @@
 function ProjectCard({ entry }) {
-
-	const featuredImageUrl = entry.featuredImage.fields.file.url;
+  const featuredImageUrl = entry.featuredImage.fields.file.url;
 
   return (
-		<div className="project-card rounded overflow-hidden">
-			<div className="aspect-video overflow-hidden">
-				<img src={featuredImageUrl} alt={entry.title} />
-			</div>
-			<div className="bg-black/10">
-				<h2 className="p-2 text-xl">{entry.title}</h2>
-			</div>
-		</div>
-	);
+    <div className='project-card rounded overflow-hidden'>
+      <div className='aspect-video overflow-hidden'>
+        <img src={featuredImageUrl} alt={entry.title} />
+      </div>
+      <div className='bg-zinc-800'>
+        <p className='p-2 text-xs text-stone-300 text-start font-normal uppercase ml-4'>
+          click here to visit
+        </p>
+        <h2 className='p-2 text-xl text-white text-start uppercase ml-4'>
+          {entry.title}
+        </h2>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectCard;
